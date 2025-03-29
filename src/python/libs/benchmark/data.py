445892e2e6f7088b01dcbefe52470c198934717d
Arthur Negrão <arthurnegrao10s@gmail.com>
2025-03-29 01:57:04 -0300

    Criado a base dos benchmarks em python

---
+++ src/python/libs/benchmark/data.py
@@ -0,0 +1,10 @@
+# Definig metrics
+class BenchMatrics:
+    """
+    Class to storage data of simulation
+    """
+    memory_usage: int
+    execution_time: float
+    steps: int
+    label: str
+
