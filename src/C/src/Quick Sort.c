0531164779fa9ab0ea02699baff06259d9d0b5a4
ofranbraga <franciscocbbraga@gmail.com>
2025-03-29 18:48:43 -0300

    Adicionei o void generate_data no projeto Quick Sort em c >> Geradopr de dados para testes.

--- src/C/src/Quick Sort.c
+++ src/C/src/Quick Sort.c
@@ -37,5 +37,26 @@ void quick_sort_helper(int arr[], int low, int high, int *comparisons, int *swap
 void quick_sort(int arr[], int n, int *comparisons, int *swaps){
     *comparisons = 0;
     *swaps =0;
-    quick_sort_helper(arr, 0, n - 1, comparisons, swaps):
+    quick_sort_helper(arr, 0, n - 1, comparisons, swaps);
 }
+
+void generate_data(int arr[], int size, const char* data_type){
+    if (strcmp(data_type, "sorted") == 0) {
+        for (int i = 0; i < size; i++) {
+            arr[i] = i;
+        }
+    } else if (strcmp(data_type, "reverse_sorted") == 0) {
+        for (int i = 0; i < size; i++) {
+            arr[i] = size - i;
+        }
+    } else if (strcmp(data_type, "random") == 0) {
+        for (int i = 0; i < size; i++) {
+            arr[i] = rand() % size;
+        }
+    } else if (strcmp(data_type, "many_duplicates") == 0) {
+        for (int i = 0; i < size; i++) {
+            arr[i] = rand() % (size / 10);
+        }
+    }
+}
+
