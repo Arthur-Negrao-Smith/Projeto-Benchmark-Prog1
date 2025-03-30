9de9461856db3608f1859f39fd8031fd3c5dc583
Arthur Negrão <arthurnegrao10s@gmail.com>
2025-03-30 11:57:01 -0300

    Adicionada documentação ao bubble sort: comentários e docstrings

--- src/python/libs/order_algorithms/bubble_sort.py
+++ src/python/libs/order_algorithms/bubble_sort.py
@@ -7,7 +7,6 @@ if platform == "win32":
 else:
     path.append(f"{python_path}/src/python/libs")
 
-print(f"Aqui está o path: {path}")
 
 from basic_elements import ListCreator
 from benchmark.data import BenchMetrics
@@ -15,19 +14,28 @@ from benchmark.measuring import benchmark
 
 import logging
 
-class Bubble(ListCreator):
+class BubbleSort(ListCreator):
     def __init__(self) -> None:
         super().__init__()
 
     @benchmark
-    def sort(self, metrics: BenchMetrics) -> int:
+    def sort(self, metrics: BenchMetrics) -> BenchMetrics:
+        """
+        Will sort all number in the list with bubble sort algorithm
+
+        Args:
+            metrics (required): Metrics wich will be used to storage data
+
+        Returns: 
+            BenchMetrics: All metrics updated
+        """
 
         if len(self.numbers_list) == 0:
             logging.warning("Lista de números está vazia. Impossível ordenar")
             metrics.label="Erro"
             return metrics
         
-        counter: int = 0
+        counter: int = 0 # Counter to count steps
         sorted: bool = False
         list_size: int = len(self.numbers_list)
 
@@ -35,34 +43,39 @@ class Bubble(ListCreator):
             
             for n in range(list_size):
                    
-                counter += 1
+                counter += 1 # Update steps
+
+                # If n is index of the last element
                 if  n == list_size - 1:
                     sorted = True
                     break
-               
+                
+                # Swap values
                 elif self.numbers_list[n] > self.numbers_list[n+1]:
                    temp: int = self.numbers_list[n]
                    self.numbers_list[n] = self.numbers_list[n+1]
                    self.numbers_list[n+1] = temp
                    break
 
+                # Just ignore
                 elif self.numbers_list[n] <= self.numbers_list[n+1]:
                     continue
 
+                # Debug
                 else:
                     logging.warning("Comportamento inesperado no Bubble sort")
                
-        metrics.steps = counter
+        metrics.steps = counter # Update steps on metrics
         return metrics
     
 
 if __name__ == "__main__":
     
     from random import randint
-    bubble: Bubble = Bubble()
+
+    bubble: BubbleSort = BubbleSort()
     bubble.numbers_list = [randint(0, 100) for c in range(100)]
 
-    
     bubble.print_list()
     metrics: BenchMetrics = BenchMetrics()
     metrics.label = "Teste"
