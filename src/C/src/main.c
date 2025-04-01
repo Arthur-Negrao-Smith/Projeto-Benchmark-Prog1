8fc1445eedc7d54e1a1dc7c708b0485d76a5b95d
Arthur Negrão <arthurnegrao10s@gmail.com>
2025-04-01 18:47:32 -0300

    Main foi atualizada para comportar as novas funções do bubble sort em c

--- src/C/src/main.c
+++ src/C/src/main.c
@@ -1,14 +1,32 @@
 #include "benchmark.h"
 #include "bubble_sort.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    char label[MAX_STRING_SIZE] = "Teste de BenchMetrics";
-    BenchMetrics *metrics = create_BenchMetrics(label);
-    printf("%s", metrics->label);
+    char algorithm_name[MAX_ALGORITHM_NAME_SIZE] = "Teste Algoritmo";
+    char data_type[MAX_DATA_TYPE_SIZE] = "Teste tipo";
+    long int size = FIRST_SIZE;
+    BenchMetrics *metrics = create_BenchMetrics(algorithm_name, data_type, size);
+    printf("Nome do algoritmo: %s\tNome do tipo: %s\tTamanho do array: %ld\n", metrics->algorithm_name, metrics->data_type, metrics->array_size);
+    free(metrics);
 
     // Tests
-    test_bubble_sort();
+    BenchMetrics *benchmetrics_array[TOTAL_METRICS_POSSIBLES];
+    benchmark_bubble_sort(benchmetrics_array);
+    printf("\nTeste do array de dados:\n");
+    printf("1. Nome do algoritmo: %s\tNome do tipo: %s\t\tTamanho do array: %ld\n", 
+        benchmetrics_array[0]->algorithm_name,
+        benchmetrics_array[0]->data_type,
+        benchmetrics_array[0]->array_size
+    );
+    printf("2. Nome do algoritmo: %s\tNome do tipo: %s\t\tTamanho do array: %ld\n", 
+        benchmetrics_array[1]->algorithm_name,
+        benchmetrics_array[1]->data_type,
+        benchmetrics_array[1]->array_size
+    );
+
+    free_BenchMetrics_array(benchmetrics_array);
     return 0;
 }
