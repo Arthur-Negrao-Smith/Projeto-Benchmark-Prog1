ab8a344ff3aaa60622e07567e944d25331ce1f3d
Arthur Negrão <arthurnegrao10s@gmail.com>
2025-04-02 18:32:13 -0300

    Agora Quick Sort está adaptado as mundanças de BenchMetrics no python

--- src/python/libs/order_algorithms/quick_sort.py
+++ src/python/libs/order_algorithms/quick_sort.py
@@ -1,26 +1,26 @@
+# Update path to libs
 from os import getcwd
-from sys import path, platform
+from basic_elements import update_python_path
+current_dir: str = getcwd()
+update_python_path(current_dir)
 
-python_path: str = getcwd()
-if platform == "win32":
-    path.append(f"{python_path}\\src\\python\\libs")
-else:
-    path.append(f"{python_path}/src/python/libs")
-
-
+# Extern imports
+from typing import Union
 import logging
 
+# Inter imports
 from basic_elements import ListCreator 
-from benchmark.data import BenchMetrics
+from benchmark.data import BenchMetrics, QUICK_NAME, FIRST_ORDER
 from benchmark.measuring import benchmark
 
 class QuickSort(ListCreator):
     def __init__(self) -> None:
         super().__init__()
-        self.steps: int = 0 # to calculate steps
+        self.comparations: int = 0 # to calculate comparations
+        self.swaps: int = 0 # To calculate swaps
 
     @benchmark
-    def benchmarkSort(self, metrics: BenchMetrics) -> BenchMetrics:
+    def benchmarkSort(self, metrics: BenchMetrics) -> Union[BenchMetrics | None]:
         """
         Will sort all number in the list with quick sort algorithm
 
@@ -29,18 +29,19 @@ class QuickSort(ListCreator):
 
         Returns: 
             BenchMetrics: All metrics updated
+            None: If list is empty
         """
         
         if len(self.numbers_list) == 0:
             logging.warning("Lista de números está vazia. Impossível ordenar")
-            metrics.label = "Erro: Lista Vazia"
-            metrics.execution_time = 0
-            metrics.memory_usage = (0, 0)
-            metrics.steps = 0
-            return metrics
+            return None
         
+        # Run Algorithm 
         self.quickSort(self.numbers_list, 0, len(self.numbers_list) - 1)
-        metrics.steps = self.steps # to calculate steps
+        
+        # Update metrics
+        metrics.comparations = self.comparations # Update comparations on metrics
+        metrics.swaps = self.swaps # Update swaps on metrics
         return metrics
     
     def swap(self, numbers_list: list[int], a_index: int, b_index: int) -> None:
@@ -89,32 +90,48 @@ class QuickSort(ListCreator):
         r: int = right
         pivot: int = numbers_list[right]
 
+
+        # To avoid unnecessary increments
+        if not (l < r):
+            self.comparations += 1
         while l < r:
 
+
+            # To avoid unnecessary increments
+            if not (l < right and numbers_list[l] < pivot):
+                self.comparations += 1
             # If left index is less than right index and left element is less than pivot
             while l < right and numbers_list[l] < pivot:
                 l += 1
-                self.steps += 1
+                self.comparations += 1
 
+
+            # To avoid unnecessary increments
+            if not (r > left and numbers_list[r] >= pivot):
+                self.comparations += 1
             # If right index is greater than left index and right element is greater than pivot
             while r > left and numbers_list[r] >= pivot:
                 r -= 1
-                self.steps += 1
+                self.comparations += 1
 
             # If left index is less than right index, then swap them
             if l < r:
                 self.swap(numbers_list=numbers_list, a_index=l, b_index=r)
-                self.steps += 1
+                self.swaps += 1
 
+
+        # To avoid unnecessary increments
+        if not (numbers_list[l] > pivot):
+            self.comparations += 1
         # If l element is grates than pivot, then swap then
         if numbers_list[l] > pivot:
             self.swap(numbers_list=numbers_list, a_index=l, b_index=right)
-            self.steps += 1
+            self.swaps += 1
 
         return l
 
     @benchmark
-    def qsortBenchmark(self, metrics: BenchMetrics) -> BenchMetrics:
+    def qsortBenchmark(self, metrics: BenchMetrics) -> Union[BenchMetrics | None]:
         """
         Will sort all number in the list with quick sort algorithm
 
@@ -126,14 +143,14 @@ class QuickSort(ListCreator):
         """
         if len(self.numbers_list) == 0:
             logging.warning("Lista de números está vazia. Impossível ordenar")
-            metrics.label = "Erro: Lista Vazia"
-            metrics.execution_time = 0
-            metrics.memory_usage = (0, 0)
-            metrics.steps = -1
-            return metrics
+            return None
         
+        # Run Algorithm 
         sorted(self.numbers_list)
-        metrics.steps = -1 # to calculate steps
+
+        # Update metrics
+        metrics.comparations = -1 # Update comparations on metrics
+        metrics.swaps = -1 # Update swaps on metrics
         return metrics
 
 
@@ -143,12 +160,19 @@ if __name__ == "__main__":
 
     quick: QuickSort = QuickSort()
     quick.numbers_list = [randint(0, 100) for c in range(100)]
-    
+
     quick.print_list()
-    metrics: BenchMetrics = BenchMetrics()
-    metrics.label = "Teste"
-    metrics.steps = 0
-    metrics = quick.benchmarkSort(metrics)
-    quick.print_list()
-    print(f"\nLabel: {metrics.label}, Steps: {metrics.steps}, Memory: {metrics.memory_usage}, Execution time: {metrics.execution_time}")
-    
+    metrics: BenchMetrics | None = BenchMetrics()
+    if (metrics != None):
+        metrics.algorithm_name = QUICK_NAME
+        metrics.data_type = FIRST_ORDER
+        metrics = quick.benchmarkSort(metrics, )
+        quick.print_list()
+        print(f"""
+          Algotithm Name: {metrics.algorithm_name} 
+          Data Type: {metrics.data_type}
+          Execution time: {metrics.execution_time}
+          Memory: {metrics.memory_usage}
+          Comparations: {metrics.comparations}
+          Swaps: {metrics.swaps}
+        """)
